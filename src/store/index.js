1d39b1afa095feaa7009e9430eef084245db3534
wenting <1570846807@qq.com>
2022-11-06 20:41:41 +0800

    ok

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,36 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import cun from 'vuex-persistedstate';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    cart:[],
   },
   getters: {
+    
   },
   mutations: {
+    ye(state,newVal){
+      state.cart=newVal
+    },
+    add(state,obj){
+      state.cart.push(obj)
+      console.log(state.cart);
+    },
+    del(state,idx){
+      state.cart.splice(idx,1)
+
+    }
   },
   actions: {
   },
   modules: {
-  }
+  },
+  plugins:[
+    cun({
+      
+    })
+  ]
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,179 @@
+<!--  -->
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>CasshBook</h1>
+    <p>你的余额</p>
+    <h1>${{ changeNum.toFixed(2) }}</h1>
+    <div class="box">
+      <div class="green">
+        <span>收入</span>
+        <span>${{ changeZum.toFixed(2) }}</span>
+      </div>
+      <div class="red">
+        <span>支出</span>
+        <span>${{ changeFum.toFixed(2) }}</span>
+      </div>
+    </div>
+
+    <h2>历史记录:{{ cart.length }}条</h2>
+    <ul>
+      <li v-for="(item, index) in cart" :key="index">
+        <span>{{ item.name }}</span>
+        <span :class="item.price > 0 ? 'green' : 'red'">{{ item.price }}</span>
+        <div class="XX" @click="del(index)">X</div>
+      </li>
+    </ul>
+
+    <h3>添加新交易</h3>
+    <div>
+      <h4>交易名称</h4>
+      <input type="text" v-model="name" @keyup.enter="add" />
+      <h4>金额</h4>
+      <input type="text" v-model="price" @keyup.enter="add" />
+    </div>
+    <button @click="add">添加新交易</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { mapMutations, mapState } from "vuex";
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  name: "Com",
+  data() {
+    return {
+      name: "",
+      price: "",
+    };
+  },
+
+  components: {},
+
+  computed: {
+    ...mapState(["cart"]),
+    changeNum() {
+      let changeSum = 0;
+      this.cart.map((item, index) => {
+        changeSum += item.price * item.num;
+      });
+      return changeSum;
+    },
+    changeZum() {
+      let changeSum = 0;
+      this.cart.map((item, index) => {
+        if (item.price > 0) {
+          changeSum += item.price * item.num;
+        }
+      });
+      return changeSum;
+    },
+    changeFum() {
+      let changeSum = 0;
+      this.cart.map((item, index) => {
+        if (item.price < 0) {
+          changeSum += item.price * item.num;
+        }
+      });
+      return changeSum;
+    },
+  },
+
+  mounted() {},
+
+  methods: {
+    ...mapMutations(["ye"]),
+
+    add() {
+      if (this.name == "" || this.price == "") {
+        alert("不能为空");
+      } else {
+        this.$store.commit("add", {
+          name: this.name,
+          price: this.price,
+          num: 1,
+        });
+        this.name = "";
+        this.price = "";
+      }
+    },
+    del(idx) {
+      this.$store.commit("del", idx);
+    },
+  },
+  watch: {
+    cart: {
+      deep: true,
+      handler(v) {
+        this.ye(v);
+      },
+    },
+  },
+  mixins: [],
+};
+</script>
+<style lang="scss" scoped>
+.box {
+  width: 350px;
+  height: 100px;
+  display: flex;
+  background: white;
+  margin: auto;
+  div {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
 }
-</script>
+ul {
+  width: 350px;
+  margin: auto;
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  li {
+    width: 350px;
+    height: 40px;
+    background: white;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 5px 0;
+    position: relative;
+    span {
+      margin: 0 15px;
+    }
+    .XX {
+      width: 25px;
+      height: 25px;
+      background-color: red;
+      color: white;
+      position: absolute;
+      left: -25px;
+      display: none;
+      line-height: 25px;
+    }
+  }
+  li:hover .XX {
+    display: block;
+  }
+}
+input {
+  width: 350px;
+  margin: 5px 0;
+  height: 40px;
+}
+button {
+  margin: 5px 0;
+  width: 350px;
+  height: 40px;
+}
+.red {
+  color: red !important;
+}
+.green {
+  color: green !important;
+}
+</style>
